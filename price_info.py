33b6a1f21f81365acb53c11fa148442fbae63328
Jing Tao <jingtao.23@ichat.sp.edu.sg>
2024-11-13 23:40:47 +0800

    Added a new price_info file and test_price_info file for Lab 3 Exercise 4

--- price_info.py
+++ price_info.py
@@ -4,28 +4,39 @@ price_list={'apple' : 1.20, 'orange':1.40, 'watermelon': 6.50, 'pineapple': 2.70
 quantity_list= {'apple': 5, 'orange':5, 'watermelon': 1, 'pineapple': 2, 'pear' : 10, 'papaya': 1, 'pomegranate': 2}
 
 
+def print_fruit_price_and_quantity(whatfruit):
+    fruitprice = price_list[whatfruit]
+    fruitquantity = quantity_list[whatfruit]
+    print("Price of "+whatfruit+" is ", fruitprice)
+    print("Quantity to buy is ", fruitquantity)
+
+
 def total_cost_shopping():
     total_cost = 0
     for key in price_list.keys():
         if key in quantity_list:
             # complete the implementation below:
+            total_cost += ( price_list[key] * quantity_list[key] )
 
     print("total cost = ", total_cost)
+    return total_cost #add this
 
 
 def cost_of_fruits(fruit, quantity):
     for key in price_list.keys():
-        if key == fruit:
+        if key == fruit: #check for correct fruit input
             cost = quantity*price_list[key]
             break
 
     print("cost of ", quantity, fruit, "=", cost)
+    return cost #add this for test file
 
 
 def main():
 
     cost_of_fruits('apple', 10)
     total_cost_shopping()
+    print_fruit_price_and_quantity("pear")
 
 
 if __name__ == "__main__":
